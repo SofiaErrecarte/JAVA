0531619d6318d53da6f91635e136d4b4866d6187
SofiaErrecarte <57240379+SofiaErrecarte@users.noreply.github.com>
2020-06-03 16:40:44 -0300

    Update Principal.java

--- tp2/Principal.java
+++ tp2/Principal.java
@@ -2,8 +2,9 @@ package tp2;
 
 public class Principal {
 	
-	static Usuario[] usuarios = crearUsuarios();
+	static Usuario[] usuarios;
 	public static void main(String[] args) {
+		usuarios=Usuario.inicializarUsuarios();
 		Ventana ventana = new Ventana();
 		ventana.crearVentana();
 		ventana.setTitle("Bienvenido al banco");
@@ -13,13 +14,4 @@ public class Principal {
 		
 	}
 
-	public static Usuario[] crearUsuarios() {
-		Usuario[] users = {
-   			 new Usuario("Sofia1234", "passwor1", "100", "500", "123", "850"),
-   			 new Usuario("Dolores1234", "passwor2", "200", "700", "456", "470"),
-   			 new Usuario("Camila1234", "passwor3", "300", "900", "789", "788"),
-   			 new Usuario("Patricia1234", "passwor4", "400", "1250", "444", "999")
-   			};
-   	return users;
-	}
 }
