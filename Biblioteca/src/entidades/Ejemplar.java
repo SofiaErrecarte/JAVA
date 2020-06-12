9612c09902c8517a669a9e47e395eca8ca3d4faa
SofiaErrecarte <57240379+SofiaErrecarte@users.noreply.github.com>
2020-06-12 17:17:34 -0300

    Add files via upload

---
+++ Biblioteca/src/entidades/Ejemplar.java
@@ -0,0 +1,13 @@
+package entidades;
+
+public class Ejemplar {
+	private int idEjemplar;
+
+	public int getIdEjemplar() {
+		return idEjemplar;
+	}
+
+	public void setIdEjemplar(int idEjemplar) {
+		this.idEjemplar = idEjemplar;
+	}
+}
---
+++ Biblioteca/src/entidades/NoSocio.java
@@ -0,0 +1,13 @@
+package entidades;
+
+public class NoSocio extends Persona{
+	private float montoAPagar;
+
+	public float getMontoAPagar() {
+		return montoAPagar;
+	}
+
+	public void setMontoAPagar(float montoAPagar) {
+		this.montoAPagar = montoAPagar;
+	}
+}
