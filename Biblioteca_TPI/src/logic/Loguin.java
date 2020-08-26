7ea41c2c448206bd2b1e98fe3f649e4fdd614a31
SofiaErrecarte <57240379+SofiaErrecarte@users.noreply.github.com>
2020-08-26 17:32:39 -0300

    Update Loguin.java

--- Biblioteca_TPI/src/logic/Loguin.java
+++ Biblioteca_TPI/src/logic/Loguin.java
@@ -1,5 +1,20 @@
 package logic;
 
-public class Loguin {
+import java.util.LinkedList;
 
+import data.DataPoliticaPrestamo;
+import entities.Persona;
+import entities.PoliticaPrestamo;
+
+public class Login {
+	private DataPoliticaPrestamo dpp;
+	public Login() {
+		dpp=new DataPoliticaPrestamo();
+	}
+	public LinkedList<PoliticaPrestamo> ppGetAll(){
+		return dpp.getAll();
+	}
+	public PoliticaPrestamo newPerson(PoliticaPrestamo pp) {
+		return dpp.add(pp);
+	}
 }
